ce1671fbf295df5d7be17f89d46d7c79ebd953f3
Teti <rohatetiana@gmail.com>
2026-01-13 09:20:11 +0100

    resolve coments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,14 @@ function calculateRentalCost(days) {
   const BASE_PRICE = 40;
   const THREE_DAYS_DISCOUNT = 20;
   const SEVEN_DAYS_DISCOUNT = 50;
+  const LONG_TERM = 7;
+  const MEDIUM_RENTAL_PERIOD = 3;
 
-  if (days < 3) {
+  if (days < MEDIUM_RENTAL_PERIOD) {
     return days * BASE_PRICE;
   }
 
-  if (days >= 3 && days <= 6) {
+  if (days >= MEDIUM_RENTAL_PERIOD && days < LONG_TERM) {
     return days * BASE_PRICE - THREE_DAYS_DISCOUNT;
   }
 
